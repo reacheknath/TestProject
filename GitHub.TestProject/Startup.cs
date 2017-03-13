60bacf4b3b0f7501aaa2baa11d876d6d4c7c1309
Ek D <eknath.damireddy@theaa.com>
2017-03-13 16:29:17 +0000

    Test commit

---
+++ GitHub.TestProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitHub.TestProject.Startup))]
+namespace GitHub.TestProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
